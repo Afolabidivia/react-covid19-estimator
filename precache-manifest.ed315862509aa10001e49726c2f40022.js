8efcdbea8114dd9c563fe2ff5cf56b60dbf5e81b
devDavid <princediviva4xuccess@gmail.com>
2020-04-14 12:06:49 +0100

    Updates

---
+++ precache-manifest.ed315862509aa10001e49726c2f40022.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999f0602263f6e098e5eed640b36bd7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54c835d634d1fea8ca94",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49a374c106354a4b475f",
+    "url": "/static/js/2.44ca3e0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44ca3e0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c835d634d1fea8ca94",
+    "url": "/static/js/main.2a438349.chunk.js"
+  },
+  {
+    "revision": "9460e7f108effcccb45d",
+    "url": "/static/js/runtime-main.6709a6cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
